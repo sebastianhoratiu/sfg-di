6aac4217cb233f0a01fb7aea7e0f0e1846683cb0
Sebastian Suciu <sebastian.suciu@garmin.com>
2023-11-23 10:39:58 +0200

    Lesson 39 - Default Profile. The default profile runs whenever you don't specify another profile. BUT, in order for a bean to be picked up by the default profile, you need to explicitly specify it via the Profile adnotation. You can also add multiple profiles to a component, like we've don for the spanish one and then that component will be used by all those profile, depending on which is running.

--- src/main/java/com/example/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/com/example/sfgdi/services/I18nSpanishGreetingService.java
@@ -3,7 +3,7 @@ package com.example.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("SP")
+@Profile({"SP","default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingsService {
     @Override

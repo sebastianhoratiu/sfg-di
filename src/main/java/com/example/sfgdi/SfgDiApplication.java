cb5308fc96b7badf775eeb792c022f7d12c6a9fa
Sebastian <sebastianhoratiu@gmail.com>
2023-12-18 09:22:29 +0200

    Lesson-41 Spring Bean Life Cycle Demo. You can do custom things to a bean while it is being constructed, in different stages of this process. You can see bean name, set application context, do things before and after it is initialized, etc. by implementing specific interfaces. I wonder if you could not just do all this by simple code directly in the constructor...

--- src/main/java/com/example/sfgdi/SfgDiApplication.java
+++ src/main/java/com/example/sfgdi/SfgDiApplication.java
@@ -1,9 +1,12 @@
 package com.example.sfgdi;
 
 import com.example.sfgdi.controllers.*;
+import com.example.sfgdi.services.LifeCycleDemoBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class SfgDiApplication {
---
+++ src/main/java/com/example/sfgdi/services/LifeCycleDemoBean.java
@@ -0,0 +1,60 @@
+package com.example.sfgdi.services;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+public class LifeCycleDemoBean implements InitializingBean, DisposableBean, BeanNameAware, BeanFactoryAware, ApplicationContextAware {
+    public LifeCycleDemoBean() {
+        System.out.println("## I'm in the LifeCycleDemoBean Constructor");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("## The LifeCycleDemoBean has its properties set!");
+    }
+
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("## The bean has been destroyed!");
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("## My Bean Name is:" + s);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("## Bean factory has been set!");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("## Application context has been set!");
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("## The @PostConstruct annotated method has been called");
+    }
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("## The @PreDestroy annotated method has been called.");
+    }
+
+    public void beforeInit() {
+        System.out.println("## beforeInit - called by bean PostProcessor");
+    }
+
+    public void afterInit() {
+        System.out.println("## afterInit - called by bean PostProcessor");
+    }
+}
